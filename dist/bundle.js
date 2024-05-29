50ae6e9c0a86b52618c6ceeeff58bd8d657998d3
Ben-Long50 <benjlong50@gmail.com>
2024-05-28 17:41:16 -0700

    added links and placeholder info for project cards

--- dist/bundle.js
+++ dist/bundle.js
@@ -25,10 +25,12 @@ __webpack_require__.r(__webpack_exports__);
 var ___CSS_LOADER_URL_IMPORT_0___ = new URL(/* asset import */ __webpack_require__(/*! ../assets/github.svg */ "./src/assets/github.svg"), __webpack_require__.b);
 var ___CSS_LOADER_URL_IMPORT_1___ = new URL(/* asset import */ __webpack_require__(/*! ../assets/linkedin.svg */ "./src/assets/linkedin.svg"), __webpack_require__.b);
 var ___CSS_LOADER_URL_IMPORT_2___ = new URL(/* asset import */ __webpack_require__(/*! ../assets/twitter.svg */ "./src/assets/twitter.svg"), __webpack_require__.b);
+var ___CSS_LOADER_URL_IMPORT_3___ = new URL(/* asset import */ __webpack_require__(/*! ../assets/open-in-new.svg */ "./src/assets/open-in-new.svg"), __webpack_require__.b);
 var ___CSS_LOADER_EXPORT___ = _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_1___default()((_node_modules_css_loader_dist_runtime_sourceMaps_js__WEBPACK_IMPORTED_MODULE_0___default()));
 var ___CSS_LOADER_URL_REPLACEMENT_0___ = _node_modules_css_loader_dist_runtime_getUrl_js__WEBPACK_IMPORTED_MODULE_2___default()(___CSS_LOADER_URL_IMPORT_0___);
 var ___CSS_LOADER_URL_REPLACEMENT_1___ = _node_modules_css_loader_dist_runtime_getUrl_js__WEBPACK_IMPORTED_MODULE_2___default()(___CSS_LOADER_URL_IMPORT_1___);
 var ___CSS_LOADER_URL_REPLACEMENT_2___ = _node_modules_css_loader_dist_runtime_getUrl_js__WEBPACK_IMPORTED_MODULE_2___default()(___CSS_LOADER_URL_IMPORT_2___);
+var ___CSS_LOADER_URL_REPLACEMENT_3___ = _node_modules_css_loader_dist_runtime_getUrl_js__WEBPACK_IMPORTED_MODULE_2___default()(___CSS_LOADER_URL_IMPORT_3___);
 // Module
 ___CSS_LOADER_EXPORT___.push([module.id, `body {
   --fs-1: 2rem;
@@ -36,7 +38,7 @@ ___CSS_LOADER_EXPORT___.push([module.id, `body {
   --fs-3: 1rem;
   --icon-1: 3rem;
   --icon-2: 2rem;
-  --icon-3: 1rem;
+  --icon-3: 1.5rem;
   --box-shadow: 4px 4px 10px 0px rgba(0, 0, 0, 0.7);
   --accent-color: rgb(92, 168, 92);
   --bg-lvl-1: #121212;
@@ -67,7 +69,12 @@ ___CSS_LOADER_EXPORT___.push([module.id, `body {
 }
 
 .icon-3 {
-  font-size: var(--icon-3);
+  display: block;
+  height: var(--icon-3);
+  width: var(--icon-3);
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center;
 }
 
 .icon-dark {
@@ -111,7 +118,7 @@ ___CSS_LOADER_EXPORT___.push([module.id, `body {
 
 .header-image {
   box-shadow: var(--box-shadow);
-  width: 30%;
+  width: 300px;
   height: auto;
   z-index: 1;
 }
@@ -160,12 +167,17 @@ ___CSS_LOADER_EXPORT___.push([module.id, `body {
 .linkedin {
   mask-image: url(${___CSS_LOADER_URL_REPLACEMENT_1___});
 }
+
 .twitter {
   mask-image: url(${___CSS_LOADER_URL_REPLACEMENT_2___});
 }
 
+.live-preview {
+  mask-image: url(${___CSS_LOADER_URL_REPLACEMENT_3___});
+}
+
 .main {
-  margin: 4rem 0;
+  margin: 6rem 0;
   display: flex;
   justify-content: center;
 }
@@ -191,28 +203,41 @@ ___CSS_LOADER_EXPORT___.push([module.id, `body {
 .project-card {
   display: flex;
   flex-direction: column;
-  aspect-ratio: 4 / 5;
   background-color: var(--bg-lvl-2);
   box-shadow: var(--box-shadow);
 }
 
 .screenshot {
   width: 100%;
-  flex-grow: 1;
+  height: 50%;
 }
 
 .project-info {
   width: 100%;
-  height: 40%;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  gap: 0.5rem;
+  box-sizing: border-box;
+  padding: 1.5rem;
 }
 
 .project-title {
+  font-size: var(--fs-2);
+  justify-self: start;
 }
 .project-buttons {
+  display: flex;
+  gap: 1rem;
+  justify-self: end;
 }
+
 .link {
 }
+
 .project-description {
+  font-size: var(--fs-3);
+  grid-column: 1 / 3;
+  line-height: 1.25rem;
 }
 
 .footer {
@@ -234,12 +259,12 @@ ___CSS_LOADER_EXPORT___.push([module.id, `body {
 }
 
 .footer-info {
-  height: 100%;
   background-color: var(--bg-lvl-2);
   flex-grow: 1;
   padding: 1.5rem;
   display: flex;
   flex-direction: column;
+  justify-content: space-between;
   gap: 1rem;
 }
 
@@ -249,6 +274,7 @@ ___CSS_LOADER_EXPORT___.push([module.id, `body {
 
 .footer-desc {
   font-size: var(--fs-3);
+  line-height: 1.5rem;
 }
 
 .address {
@@ -263,6 +289,7 @@ ___CSS_LOADER_EXPORT___.push([module.id, `body {
 .contact-info {
   font-size: var(--fs-3);
 }
+
 .footer-buttons {
   display: flex;
   gap: 1rem;
@@ -271,7 +298,7 @@ ___CSS_LOADER_EXPORT___.push([module.id, `body {
 .footer-image {
   width: 30%;
 }
-`, "",{"version":3,"sources":["webpack://./src/styles/main.css"],"names":[],"mappings":"AAAA;EACE,YAAY;EACZ,cAAc;EACd,YAAY;EACZ,cAAc;EACd,cAAc;EACd,cAAc;EACd,iDAAiD;EACjD,gCAAgC;EAChC,mBAAmB;EACnB,qCAAqC;AACvC;;AAEA;EACE,iCAAiC;EACjC,YAAY;AACd;;AAEA;EACE,cAAc;EACd,qBAAqB;EACrB,oBAAoB;EACpB,sBAAsB;EACtB,4BAA4B;EAC5B,2BAA2B;AAC7B;;AAEA;EACE,cAAc;EACd,qBAAqB;EACrB,oBAAoB;EACpB,sBAAsB;EACtB,4BAA4B;EAC5B,2BAA2B;AAC7B;;AAEA;EACE,wBAAwB;AAC1B;;AAEA;EACE,uBAAuB;AACzB;;AAEA;EACE,yBAAyB;AAC3B;;AAEA;EACE,aAAa;EACb,sBAAsB;EACtB,sBAAsB;EACtB,cAAc;AAChB;;AAEA;;;EAGE,sBAAsB;EACtB,eAAe;AACjB;;AAEA;EACE,sBAAsB;EACtB,eAAe;EACf,aAAa;EACb,uBAAuB;EACvB,qCAAqC;EACrC,oDAAoD;EACpD,YAAY;EACZ,6BAA6B;AAC/B;;AAEA;EACE,aAAa;EACb,iBAAiB;EACjB,oDAAoD;AACtD;;AAEA;EACE,6BAA6B;EAC7B,UAAU;EACV,YAAY;EACZ,UAAU;AACZ;;AAEA;EACE,cAAc;AAChB;;AAEA;EACE,YAAY;EACZ,WAAW;EACX,cAAc;EACd,iCAAiC;EACjC,6BAA6B;EAC7B,sBAAsB;EACtB,eAAe;EACf,aAAa;EACb,SAAS;EACT,sBAAsB;EACtB,8BAA8B;AAChC;;AAEA;EACE,sBAAsB;EACtB,sBAAsB;AACxB;;AAEA;EACE,sBAAsB;EACtB,iBAAiB;EACjB,WAAW;EACX,kBAAkB;EAClB,gBAAgB;AAClB;;AAEA;EACE,oBAAoB;EACpB,aAAa;EACb,WAAW;AACb;;AAEA;EACE,mDAAuC;AACzC;;AAEA;EACE,mDAAyC;AAC3C;AACA;EACE,mDAAwC;AAC1C;;AAEA;EACE,cAAc;EACd,aAAa;EACb,uBAAuB;AACzB;;AAEA;EACE,YAAY;EACZ,iBAAiB;AACnB;;AAEA;EACE,sBAAsB;EACtB,WAAW;EACX,gBAAgB;EAChB,mBAAmB;AACrB;;AAEA;EACE,aAAa;EACb,qCAAqC;EACrC,SAAS;AACX;;AAEA;EACE,aAAa;EACb,sBAAsB;EACtB,mBAAmB;EACnB,iCAAiC;EACjC,6BAA6B;AAC/B;;AAEA;EACE,WAAW;EACX,YAAY;AACd;;AAEA;EACE,WAAW;EACX,WAAW;AACb;;AAEA;AACA;AACA;AACA;AACA;AACA;AACA;AACA;;AAEA;EACE,YAAY;EACZ,aAAa;EACb,uBAAuB;EACvB,uBAAuB;EACvB,qCAAqC;EACrC,qBAAqB;EACrB,2BAA2B;AAC7B;;AAEA;EACE,iCAAiC;EACjC,4BAA4B;EAC5B,aAAa;EACb,iBAAiB;EACjB,6BAA6B;AAC/B;;AAEA;EACE,YAAY;EACZ,iCAAiC;EACjC,YAAY;EACZ,eAAe;EACf,aAAa;EACb,sBAAsB;EACtB,SAAS;AACX;;AAEA;EACE,sBAAsB;AACxB;;AAEA;EACE,sBAAsB;AACxB;;AAEA;EACE,sBAAsB;AACxB;;AAEA;EACE,aAAa;EACb,SAAS;AACX;;AAEA;EACE,sBAAsB;AACxB;AACA;EACE,aAAa;EACb,SAAS;AACX;;AAEA;EACE,UAAU;AACZ","sourcesContent":["body {\n  --fs-1: 2rem;\n  --fs-2: 1.5rem;\n  --fs-3: 1rem;\n  --icon-1: 3rem;\n  --icon-2: 2rem;\n  --icon-3: 1rem;\n  --box-shadow: 4px 4px 10px 0px rgba(0, 0, 0, 0.7);\n  --accent-color: rgb(92, 168, 92);\n  --bg-lvl-1: #121212;\n  --bg-lvl-2: rgba(255, 255, 255, 0.05);\n}\n\n.dark-theme {\n  background-color: var(--bg-lvl-1);\n  color: white;\n}\n\n.icon-1 {\n  display: block;\n  height: var(--icon-1);\n  width: var(--icon-1);\n  background-size: cover;\n  background-repeat: no-repeat;\n  background-position: center;\n}\n\n.icon-2 {\n  display: block;\n  height: var(--icon-2);\n  width: var(--icon-2);\n  background-size: cover;\n  background-repeat: no-repeat;\n  background-position: center;\n}\n\n.icon-3 {\n  font-size: var(--icon-3);\n}\n\n.icon-dark {\n  background-color: white;\n}\n\n.icon-light {\n  background-color: #121212;\n}\n\n.content {\n  display: flex;\n  flex-direction: column;\n  box-sizing: border-box;\n  padding: 2em 0;\n}\n\n.header,\n.main,\n.footer {\n  box-sizing: border-box;\n  padding: 0 2rem;\n}\n\n.header {\n  box-sizing: border-box;\n  padding: 0 2rem;\n  display: flex;\n  justify-content: center;\n  background-color: var(--accent-color);\n  transform: scaleY(2) translateY(-35.5%) skewY(-7deg);\n  width: 100vw;\n  box-shadow: var(--box-shadow);\n}\n\n.header-container {\n  display: flex;\n  max-width: 1000px;\n  transform: translateY(35.5%) skewY(7deg) scaleY(0.5);\n}\n\n.header-image {\n  box-shadow: var(--box-shadow);\n  width: 30%;\n  height: auto;\n  z-index: 1;\n}\n\n.header-background {\n  margin: 2rem 0;\n}\n\n.header-info-container {\n  height: 100%;\n  width: 100%;\n  color: inherit;\n  background-color: var(--bg-lvl-2);\n  box-shadow: var(--box-shadow);\n  box-sizing: border-box;\n  padding: 1.5rem;\n  display: flex;\n  gap: 1rem;\n  flex-direction: column;\n  justify-content: space-between;\n}\n\n.header-title {\n  align-self: flex-start;\n  font-size: var(--fs-1);\n}\n\n.header-info {\n  font-size: var(--fs-2);\n  line-height: 2rem;\n  width: 100%;\n  align-self: center;\n  text-align: left;\n}\n\n.header-buttons {\n  align-self: flex-end;\n  display: flex;\n  gap: 0.5rem;\n}\n\n.github {\n  mask-image: url('../assets/github.svg');\n}\n\n.linkedin {\n  mask-image: url('../assets/linkedin.svg');\n}\n.twitter {\n  mask-image: url('../assets/twitter.svg');\n}\n\n.main {\n  margin: 4rem 0;\n  display: flex;\n  justify-content: center;\n}\n\n.section {\n  flex-grow: 1;\n  max-width: 1000px;\n}\n\n.project-section-title {\n  font-size: var(--fs-1);\n  width: 100%;\n  text-align: left;\n  margin-bottom: 2rem;\n}\n\n.card-container {\n  display: grid;\n  grid-template-columns: repeat(3, 1fr);\n  gap: 2rem;\n}\n\n.project-card {\n  display: flex;\n  flex-direction: column;\n  aspect-ratio: 4 / 5;\n  background-color: var(--bg-lvl-2);\n  box-shadow: var(--box-shadow);\n}\n\n.screenshot {\n  width: 100%;\n  flex-grow: 1;\n}\n\n.project-info {\n  width: 100%;\n  height: 40%;\n}\n\n.project-title {\n}\n.project-buttons {\n}\n.link {\n}\n.project-description {\n}\n\n.footer {\n  flex-grow: 1;\n  display: flex;\n  justify-content: center;\n  align-items: flex-start;\n  background-color: var(--accent-color);\n  transform-origin: top;\n  transform: translateY(4rem);\n}\n\n.footer-container {\n  background-color: var(--bg-lvl-1);\n  transform: translateY(-4rem);\n  display: flex;\n  max-width: 1000px;\n  box-shadow: var(--box-shadow);\n}\n\n.footer-info {\n  height: 100%;\n  background-color: var(--bg-lvl-2);\n  flex-grow: 1;\n  padding: 1.5rem;\n  display: flex;\n  flex-direction: column;\n  gap: 1rem;\n}\n\n.footer-title {\n  font-size: var(--fs-1);\n}\n\n.footer-desc {\n  font-size: var(--fs-3);\n}\n\n.address {\n  font-size: var(--fs-3);\n}\n\n.contact-container {\n  display: flex;\n  gap: 1rem;\n}\n\n.contact-info {\n  font-size: var(--fs-3);\n}\n.footer-buttons {\n  display: flex;\n  gap: 1rem;\n}\n\n.footer-image {\n  width: 30%;\n}\n"],"sourceRoot":""}]);
+`, "",{"version":3,"sources":["webpack://./src/styles/main.css"],"names":[],"mappings":"AAAA;EACE,YAAY;EACZ,cAAc;EACd,YAAY;EACZ,cAAc;EACd,cAAc;EACd,gBAAgB;EAChB,iDAAiD;EACjD,gCAAgC;EAChC,mBAAmB;EACnB,qCAAqC;AACvC;;AAEA;EACE,iCAAiC;EACjC,YAAY;AACd;;AAEA;EACE,cAAc;EACd,qBAAqB;EACrB,oBAAoB;EACpB,sBAAsB;EACtB,4BAA4B;EAC5B,2BAA2B;AAC7B;;AAEA;EACE,cAAc;EACd,qBAAqB;EACrB,oBAAoB;EACpB,sBAAsB;EACtB,4BAA4B;EAC5B,2BAA2B;AAC7B;;AAEA;EACE,cAAc;EACd,qBAAqB;EACrB,oBAAoB;EACpB,sBAAsB;EACtB,4BAA4B;EAC5B,2BAA2B;AAC7B;;AAEA;EACE,uBAAuB;AACzB;;AAEA;EACE,yBAAyB;AAC3B;;AAEA;EACE,aAAa;EACb,sBAAsB;EACtB,sBAAsB;EACtB,cAAc;AAChB;;AAEA;;;EAGE,sBAAsB;EACtB,eAAe;AACjB;;AAEA;EACE,sBAAsB;EACtB,eAAe;EACf,aAAa;EACb,uBAAuB;EACvB,qCAAqC;EACrC,oDAAoD;EACpD,YAAY;EACZ,6BAA6B;AAC/B;;AAEA;EACE,aAAa;EACb,iBAAiB;EACjB,oDAAoD;AACtD;;AAEA;EACE,6BAA6B;EAC7B,YAAY;EACZ,YAAY;EACZ,UAAU;AACZ;;AAEA;EACE,cAAc;AAChB;;AAEA;EACE,YAAY;EACZ,WAAW;EACX,cAAc;EACd,iCAAiC;EACjC,6BAA6B;EAC7B,sBAAsB;EACtB,eAAe;EACf,aAAa;EACb,SAAS;EACT,sBAAsB;EACtB,8BAA8B;AAChC;;AAEA;EACE,sBAAsB;EACtB,sBAAsB;AACxB;;AAEA;EACE,sBAAsB;EACtB,iBAAiB;EACjB,WAAW;EACX,kBAAkB;EAClB,gBAAgB;AAClB;;AAEA;EACE,oBAAoB;EACpB,aAAa;EACb,WAAW;AACb;;AAEA;EACE,mDAAuC;AACzC;;AAEA;EACE,mDAAyC;AAC3C;;AAEA;EACE,mDAAwC;AAC1C;;AAEA;EACE,mDAA4C;AAC9C;;AAEA;EACE,cAAc;EACd,aAAa;EACb,uBAAuB;AACzB;;AAEA;EACE,YAAY;EACZ,iBAAiB;AACnB;;AAEA;EACE,sBAAsB;EACtB,WAAW;EACX,gBAAgB;EAChB,mBAAmB;AACrB;;AAEA;EACE,aAAa;EACb,qCAAqC;EACrC,SAAS;AACX;;AAEA;EACE,aAAa;EACb,sBAAsB;EACtB,iCAAiC;EACjC,6BAA6B;AAC/B;;AAEA;EACE,WAAW;EACX,WAAW;AACb;;AAEA;EACE,WAAW;EACX,aAAa;EACb,8BAA8B;EAC9B,WAAW;EACX,sBAAsB;EACtB,eAAe;AACjB;;AAEA;EACE,sBAAsB;EACtB,mBAAmB;AACrB;AACA;EACE,aAAa;EACb,SAAS;EACT,iBAAiB;AACnB;;AAEA;AACA;;AAEA;EACE,sBAAsB;EACtB,kBAAkB;EAClB,oBAAoB;AACtB;;AAEA;EACE,YAAY;EACZ,aAAa;EACb,uBAAuB;EACvB,uBAAuB;EACvB,qCAAqC;EACrC,qBAAqB;EACrB,2BAA2B;AAC7B;;AAEA;EACE,iCAAiC;EACjC,4BAA4B;EAC5B,aAAa;EACb,iBAAiB;EACjB,6BAA6B;AAC/B;;AAEA;EACE,iCAAiC;EACjC,YAAY;EACZ,eAAe;EACf,aAAa;EACb,sBAAsB;EACtB,8BAA8B;EAC9B,SAAS;AACX;;AAEA;EACE,sBAAsB;AACxB;;AAEA;EACE,sBAAsB;EACtB,mBAAmB;AACrB;;AAEA;EACE,sBAAsB;AACxB;;AAEA;EACE,aAAa;EACb,SAAS;AACX;;AAEA;EACE,sBAAsB;AACxB;;AAEA;EACE,aAAa;EACb,SAAS;AACX;;AAEA;EACE,UAAU;AACZ","sourcesContent":["body {\n  --fs-1: 2rem;\n  --fs-2: 1.5rem;\n  --fs-3: 1rem;\n  --icon-1: 3rem;\n  --icon-2: 2rem;\n  --icon-3: 1.5rem;\n  --box-shadow: 4px 4px 10px 0px rgba(0, 0, 0, 0.7);\n  --accent-color: rgb(92, 168, 92);\n  --bg-lvl-1: #121212;\n  --bg-lvl-2: rgba(255, 255, 255, 0.05);\n}\n\n.dark-theme {\n  background-color: var(--bg-lvl-1);\n  color: white;\n}\n\n.icon-1 {\n  display: block;\n  height: var(--icon-1);\n  width: var(--icon-1);\n  background-size: cover;\n  background-repeat: no-repeat;\n  background-position: center;\n}\n\n.icon-2 {\n  display: block;\n  height: var(--icon-2);\n  width: var(--icon-2);\n  background-size: cover;\n  background-repeat: no-repeat;\n  background-position: center;\n}\n\n.icon-3 {\n  display: block;\n  height: var(--icon-3);\n  width: var(--icon-3);\n  background-size: cover;\n  background-repeat: no-repeat;\n  background-position: center;\n}\n\n.icon-dark {\n  background-color: white;\n}\n\n.icon-light {\n  background-color: #121212;\n}\n\n.content {\n  display: flex;\n  flex-direction: column;\n  box-sizing: border-box;\n  padding: 2em 0;\n}\n\n.header,\n.main,\n.footer {\n  box-sizing: border-box;\n  padding: 0 2rem;\n}\n\n.header {\n  box-sizing: border-box;\n  padding: 0 2rem;\n  display: flex;\n  justify-content: center;\n  background-color: var(--accent-color);\n  transform: scaleY(2) translateY(-35.5%) skewY(-7deg);\n  width: 100vw;\n  box-shadow: var(--box-shadow);\n}\n\n.header-container {\n  display: flex;\n  max-width: 1000px;\n  transform: translateY(35.5%) skewY(7deg) scaleY(0.5);\n}\n\n.header-image {\n  box-shadow: var(--box-shadow);\n  width: 300px;\n  height: auto;\n  z-index: 1;\n}\n\n.header-background {\n  margin: 2rem 0;\n}\n\n.header-info-container {\n  height: 100%;\n  width: 100%;\n  color: inherit;\n  background-color: var(--bg-lvl-2);\n  box-shadow: var(--box-shadow);\n  box-sizing: border-box;\n  padding: 1.5rem;\n  display: flex;\n  gap: 1rem;\n  flex-direction: column;\n  justify-content: space-between;\n}\n\n.header-title {\n  align-self: flex-start;\n  font-size: var(--fs-1);\n}\n\n.header-info {\n  font-size: var(--fs-2);\n  line-height: 2rem;\n  width: 100%;\n  align-self: center;\n  text-align: left;\n}\n\n.header-buttons {\n  align-self: flex-end;\n  display: flex;\n  gap: 0.5rem;\n}\n\n.github {\n  mask-image: url('../assets/github.svg');\n}\n\n.linkedin {\n  mask-image: url('../assets/linkedin.svg');\n}\n\n.twitter {\n  mask-image: url('../assets/twitter.svg');\n}\n\n.live-preview {\n  mask-image: url('../assets/open-in-new.svg');\n}\n\n.main {\n  margin: 6rem 0;\n  display: flex;\n  justify-content: center;\n}\n\n.section {\n  flex-grow: 1;\n  max-width: 1000px;\n}\n\n.project-section-title {\n  font-size: var(--fs-1);\n  width: 100%;\n  text-align: left;\n  margin-bottom: 2rem;\n}\n\n.card-container {\n  display: grid;\n  grid-template-columns: repeat(3, 1fr);\n  gap: 2rem;\n}\n\n.project-card {\n  display: flex;\n  flex-direction: column;\n  background-color: var(--bg-lvl-2);\n  box-shadow: var(--box-shadow);\n}\n\n.screenshot {\n  width: 100%;\n  height: 50%;\n}\n\n.project-info {\n  width: 100%;\n  display: grid;\n  grid-template-columns: 1fr 1fr;\n  gap: 0.5rem;\n  box-sizing: border-box;\n  padding: 1.5rem;\n}\n\n.project-title {\n  font-size: var(--fs-2);\n  justify-self: start;\n}\n.project-buttons {\n  display: flex;\n  gap: 1rem;\n  justify-self: end;\n}\n\n.link {\n}\n\n.project-description {\n  font-size: var(--fs-3);\n  grid-column: 1 / 3;\n  line-height: 1.25rem;\n}\n\n.footer {\n  flex-grow: 1;\n  display: flex;\n  justify-content: center;\n  align-items: flex-start;\n  background-color: var(--accent-color);\n  transform-origin: top;\n  transform: translateY(4rem);\n}\n\n.footer-container {\n  background-color: var(--bg-lvl-1);\n  transform: translateY(-4rem);\n  display: flex;\n  max-width: 1000px;\n  box-shadow: var(--box-shadow);\n}\n\n.footer-info {\n  background-color: var(--bg-lvl-2);\n  flex-grow: 1;\n  padding: 1.5rem;\n  display: flex;\n  flex-direction: column;\n  justify-content: space-between;\n  gap: 1rem;\n}\n\n.footer-title {\n  font-size: var(--fs-1);\n}\n\n.footer-desc {\n  font-size: var(--fs-3);\n  line-height: 1.5rem;\n}\n\n.address {\n  font-size: var(--fs-3);\n}\n\n.contact-container {\n  display: flex;\n  gap: 1rem;\n}\n\n.contact-info {\n  font-size: var(--fs-3);\n}\n\n.footer-buttons {\n  display: flex;\n  gap: 1rem;\n}\n\n.footer-image {\n  width: 30%;\n}\n"],"sourceRoot":""}]);
 // Exports
 /* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (___CSS_LOADER_EXPORT___);
 
@@ -979,6 +1006,16 @@ module.exports = __webpack_require__.p + "assets/img/github.aba1ece0.svg";
 /***/ ((module, __unused_webpack_exports, __webpack_require__) => {
 
 module.exports = __webpack_require__.p + "assets/img/linkedin.eaccfc4b.svg";
+
+/***/ }),
+
+/***/ "./src/assets/open-in-new.svg":
+/*!************************************!*\
+  !*** ./src/assets/open-in-new.svg ***!
+  \************************************/
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+module.exports = __webpack_require__.p + "assets/img/open-in-new.3ede19ed.svg";
 
 /***/ }),
 
